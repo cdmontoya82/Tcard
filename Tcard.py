45301743c6222d3834d356c02b3eaca8ef79933b
Cristian David Montoya Henano <cdmontoya82@gmail.com>
2025-11-05 21:32:09 -0500

    Actulizacion datos

--- Tcard.py
+++ Tcard.py
@@ -2,13 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Carga tu dataset (asegúrate de que la ruta sea correcta)
+# Cargar dataset
 df = pd.read_csv('creditcard.csv')
 
 # Vistazo rápido a las primeras filas
 print(df.head())
 
-# Revisa los tipos de datos y si hay valores nulos
+# Revisar los tipos de datos y si hay valores nulos
 print(df.info())
 
 # Estadísticas descriptivas, especialmente de 'Amount' y 'Time'
